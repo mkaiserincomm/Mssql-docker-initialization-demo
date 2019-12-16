48fa67828b58e1ec7ed7ee34a9ed4edc8a34b7db
Tometchy <tometchy@gmail.com>
2019-12-16 18:24:35 +0100

    Remove node from entrypoint script
    Without running node, starting sqlserver must be after '&' because otherwise
    container stops when import-data.sh script stops.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
-#start SQL Server, start the script to create the DB and import the data, start the app
-/opt/mssql/bin/sqlservr & /usr/src/app/import-data.sh & npm start 
+# Run Microsoft SQl Server and initialization script (at the same time)
+/usr/src/app/import-data.sh & /opt/mssql/bin/sqlservr
